df41d5b27a7594bb14a6e3c3bcaa64c0bb87cc05
aysegulyilmaz5 <yilmazansr@gmail.com>
2021-06-27 23:58:33 +0300

    Create Classes.java
    classes

---
+++ Classes.java
@@ -0,0 +1,15 @@
+package classes;
+
+class MyClass{
+	int x=5;
+	
+}
+
+public class Classes {
+	
+	public static void main(String[] args) {
+		MyClass object = new MyClass();
+		System.out.println(object.x);
+	}
+
+}
